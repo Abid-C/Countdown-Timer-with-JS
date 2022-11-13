09dcbc79633c50e6a44eca0dafcefd5377ae57e8
Abid-C <abidchowdhury56@gmail.com>
2022-11-12 19:16:41 -0500

    added a message when the countdown hits zero

--- app.js
+++ app.js
@@ -82,6 +82,11 @@ function getRemainingTime() {
     items.forEach(function(item, index) {
         item.innerHTML = format(values[index]);
     });
+
+    if (t < 0) {
+        clearInterval(countdown);
+        deadline.innerHTML = "<h4 class='expired'>sorry, this giveaway has expired!</h4>";
+    }
 }
 
 // countdown
